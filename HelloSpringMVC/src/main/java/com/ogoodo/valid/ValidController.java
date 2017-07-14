3a33512236f3ccca253974795ebc3ea68014f63c
chen <chen@chendeMacBook-Pro.local>
2017-07-14 16:36:15 +0800

    增加国际化i18n

--- HelloSpringMVC/src/main/java/com/ogoodo/valid/ValidController.java
+++ HelloSpringMVC/src/main/java/com/ogoodo/valid/ValidController.java
@@ -1,11 +1,14 @@
 package com.ogoodo.valid;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -16,13 +19,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ValidController {
 
+	@Autowired
+    private ResourceBundleMessageSource messageSource;
+	
 	/**
 	 * 验证:  http://localhost:8080/HelloSpringMVC/valid?name=chen&age=35
 	 * 		 http://localhost:8080/HelloSpringMVC/valid?name=chenchenchen&age=1
 	 */
 	@ResponseBody
 	@RequestMapping(value="/valid")
-	public Map<String,Object> saveRegistration(@Valid User user, BindingResult result){
+	public Map<String,Object> saveRegistration(@Valid User user, BindingResult result, Locale locale){
+		
+        System.out.println("testi18n:" + messageSource.getMessage("user.id.null", null, locale));
 
 		if(result.hasErrors()) {
 	        Map<String,Object> map=new HashMap<String,Object>(); 
@@ -45,6 +53,8 @@ public class ValidController {
 	        map.put("code", "10004");
 	        map.put("msg", "请求参数校验失败！！！");
 	        map.put("data", data);
+	        map.put("msgs","user.id.null");
+	        map.put("msgs2","{user.id.null}");
 	        return map;
 		}
 
